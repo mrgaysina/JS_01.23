0cd98d21ef95cf86d5c1bc074789c43cf0ca04f1
Rita <mrgaysina@gmail.com>
2023-02-15 18:40:05 +0300

    [5.2:15.02] finished second task

--- index.js
+++ index.js
@@ -61,3 +61,97 @@ class Stack {
     }
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  append(elem) {
+    if (typeof elem === 'object') {
+      elem = Object.values(elem);
+      const node = new Node(elem.toString());
+      if (this.last) {
+        this.last.next = node;
+        this.last = node;
+      } else {
+        this.last = node;
+      }
+      if (!this.first) {
+        this.first = node;
+      }
+    } else {
+      const node = new Node(elem);
+      if (this.last) {
+        this.last.next = node;
+        this.last = node;
+      } else {
+        this.last = node;
+      }
+      if (!this.first) {
+        this.first = node;
+      }
+    }
+  }
+
+  prepend(elem) {
+    if (typeof elem === 'object') {
+      elem = Object.values(elem);
+      const node = new Node(elem.toString());
+      if (this.first) {
+        node.next = this.first;
+        this.first = node;
+      } else {
+        this.first = node;
+        this.last = node;
+      }
+    } else {
+      const node = new Node(elem);
+      if (this.first) {
+        node.next = this.first;
+        this.first = node;
+      } else {
+        this.first = node;
+        this.last = node;
+      }
+    }
+  }
+
+  find(elem, current = this?.first) {
+    if (current === null) {
+      return null;
+    }
+    if (current?.value === elem) {
+      return current;
+    }
+    return this.find(elem, current?.next);
+  }
+
+  toArray(current = this.first, array = []) {
+    if (current === null) {
+      return array;
+    }
+    array.push(current);
+    return this.toArray(current.next, array);
+  }
+
+  static fromIterable(iterable) {
+    let list = new LinkedList();
+    if (typeof iterable[Symbol.iterator] !== 'function') {
+      throw new Error('Not iterable');
+    } else {
+      for (let i = 0; i < iterable.length; i++) {
+        list.append(iterable[i]);
+      }
+      return list;
+    }
+  }
+}
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
