def4313ec6843485a65b5334a239e0ac63fe1445
Rita <mrgaysina@gmail.com>
2023-02-21 22:29:47 +0300

    [6.1:21.02] correct operations

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let operand1 = '';
 let operand2 = '';
 let operator = '';
 let submit = false;
+let sliceOfOperand = false;
+let modalOfOperand = false;
+
 const screen = document.getElementsByTagName('input');
 
 function clear() {
@@ -13,11 +16,26 @@ function clear() {
 }
 
 function modal() {
+  modalOfOperand = true;
   screen[0].value = -screen[0].value;
   if (operand2 === '') {
     operand1 = -operand1;
+    operand1 = String(operand1);
   } else {
     operand2 = -operand2;
+    operand2 = String(operand2);
+  }
+}
+
+function del() {
+  sliceOfOperand = true;
+  screen[0].value = screen[0].value.slice(0, -1);
+  if (operand2 === '') {
+    operand1 = String(operand1);
+    operand1 = operand1.slice(0, -1);
+  } else {
+    operand2 = String(operand2);
+    operand2 = operand2.slice(0, -1);
   }
 }
 
@@ -29,24 +47,31 @@ for (let i = 0; i < operations.length; i += 1) {
     const pressed = event.target.textContent;
     if (pressed === 'C') clear();
     if (pressed === '+/-') modal();
+    if (pressed === 'Del') del();
     if (pressed === '=') {
       if (operand2 === '') operand2 = operand1;
       if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
         switch (operator) {
           case '+':
             operand1 = (+operand1) + (+operand2);
+            operand1 = roundNumber(operand1);
             screen[0].value = operand1;
             operand2 = '';
+            operator = '';
             break;
           case '-':
             operand1 -= operand2;
+            operand1 = roundNumber(operand1);
             screen[0].value = operand1;
             operand2 = '';
+            operator = '';
             break;
           case '*':
             operand1 *= operand2;
+            operand1 = roundNumber(operand1);
             screen[0].value = operand1;
             operand2 = '';
+            operator = '';
             break;
           case '/':
             if (operand2 === '' || operand2 === '0') {
@@ -57,8 +82,10 @@ for (let i = 0; i < operations.length; i += 1) {
               break;
             } else {
               operand1 /= operand2;
+              operand1 = roundNumber(operand1);
               screen[0].value = operand1;
               operand2 = '';
+              operator = '';
             }
             break;
           default:
@@ -67,11 +94,32 @@ for (let i = 0; i < operations.length; i += 1) {
         submit = true;
       }
     }
-    if (operand1 !== '' && operand2 !== '' && operator !== '') {
+    switch (pressed) {
+      case '+':
+        sliceOfOperand = false;
+        modalOfOperand = false;
+        break;
+      case '-':
+        sliceOfOperand = false;
+        modalOfOperand = false;
+        break;
+      case '*':
+        sliceOfOperand = false;
+        modalOfOperand = false;
+        break;
+      case '/':
+        sliceOfOperand = false;
+        modalOfOperand = false;
+        break;
+      default:
+        break;
+    }
+    if (operand1 !== '' && operand2 !== '' && operator !== '' && !sliceOfOperand && !modalOfOperand) {
       switch (operator) {
         case '+':
           operator = '+';
           operand1 = (+operand1) + (+operand2);
+          operand1 = roundNumber(operand1);
           operand2 = '';
           screen[0].value = operand1;
           operator = '';
@@ -79,6 +127,7 @@ for (let i = 0; i < operations.length; i += 1) {
         case '-':
           operator = '-';
           operand1 -= operand2;
+          operand1 = roundNumber(operand1);
           operand2 = '';
           screen[0].value = operand1;
           operator = '';
@@ -86,6 +135,7 @@ for (let i = 0; i < operations.length; i += 1) {
         case '*':
           operator = '*';
           operand1 *= operand2;
+          operand1 = roundNumber(operand1);
           screen[0].value = operand1;
           operand2 = '';
           operator = '';
@@ -100,6 +150,7 @@ for (let i = 0; i < operations.length; i += 1) {
             break;
           } else {
             operand1 /= operand2;
+            operand1 = roundNumber(operand1);
             screen[0].value = operand1;
             operand2 = '';
           }
@@ -131,17 +182,45 @@ for (let i = 0; i < operations.length; i += 1) {
 
 for (let i = 0; i < numbers.length; i += 1) {
   numbers[i].addEventListener('click', (event) => {
-    const pressed = event.target.textContent;
+    let pressed = event.target.textContent;
+    const regex = /\.\d{8,}/gm;
+
+    if (pressed === '.' && operand1.includes('.') && operator === '') {
+      return;
+    }
+    if (pressed === '.' && operand1.includes('.') && operand2.includes('.')){
+      return;
+    }
+    if ((pressed === '0' || pressed === '00') && operand1 === '') return;
     if (operand2 === '' && operator === '') {
       operand1 += pressed;
-      screen[0].value = operand1;
+      if (regex.test(String(operand1))) {
+        operand1 = roundNumber(operand1);
+        screen[0].value = operand1;
+      } else {
+        screen[0].value = operand1;
+      }
     } else if (operand1 !== '' && operand2 !== '' && submit) {
-      operand2 += pressed;
+
+      operand1 = screen[0].value;
+      operand2 = pressed;
       submit = false;
       screen[0].value = operand2;
     } else {
+
       operand2 += pressed;
-      screen[0].value = operand2;
+      if (regex.test(String(operand2))) {
+        operand2 = roundNumber(operand2);
+        screen[0].value = operand2;
+      } else {
+        screen[0].value = operand2;
+      }
     }
   });
+}
+
+function roundNumber(num) {
+  const precision = 8;
+  const rounded = Math.round(num * 10 ** precision) / 10 ** precision;
+  return String(rounded);
 }
